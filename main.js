073c147b95544d3bcf9d75e0f0138147524c467e
yulia elkina <todg@mail.ru>
2019-09-03 18:04:10 +0300

    week3-2

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-let hambmenu = document.querySelector('.hamb-menu__page');
+
+let menu = document.querySelector('.menu');
 let body = document.querySelector('body');
-let links = document.querySelectorAll('.menu__link--hamb');
+let items = document.querySelectorAll('.menu__item');
+let burger = document.querySelector('.hamb-menu__button');
 let close = document.querySelector('.hamb-menu__close');
 
+
+
+function toggleMenu(){
+  menu.classList.toggle('menu--hamb');
+  body.classList.toggle('body--active-menu');
+}
+items.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
+burger.addEventListener('click' , toggleMenu);
+close.addEventListener('click' , toggleMenu);
