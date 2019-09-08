831c45e3e7157d81915e0538bbed5f8495f73e45
yulia elkina <todg@mail.ru>
2019-09-08 23:29:45 +0300

    hw4-2

--- main.js
+++ main.js
@@ -45,12 +45,12 @@ const target = event.target;
 if (target.classList.contains("team__item-name")) {
   const worker = target.parentNode;
   const content = target.nextElementSibling;
-  const contentHeght = content.firstElementChild.clientHeight;
+  const contentHeight = content.firstElementChild.clientHeight;
 
 for (iterator of workers) {
   if (iterator !== worker) {
     iterator.classList.remove("team__item--active");
-    iterator.firstElementChild.style.height = 0;
+    iterator.lastElementChild.style.height = 0;
   }
 }
 
@@ -73,7 +73,13 @@ function popupReview() {
   const reviewList = document.querySelector('.reviews__list');
   reviewList.addEventListener("click" , function(event) {
 event.preventDefault();
-if (event.target.classList.contains('btn__read')) {
+if (event.target.classList.contains('btn__read',)) {
+  const title = event.target.parentNode.querySelector('.reviews__user-name').textContent;
+  const text = event.target.parentNode.querySelector('.reviews__text').textContent;
+
+  renderPopup(title,text);
+}
+else if (event.target.classList.contains('btn__more',)) {
   const title = event.target.parentNode.querySelector('.reviews__user-name').textContent;
   const text = event.target.parentNode.querySelector('.reviews__text').textContent;
 
@@ -95,3 +101,42 @@ function renderPopup(title,text) {
 }
 
 popupReview();
+
+
+function accordeonMenu() {
+  const menuItems = document.querySelectorAll(".menu-page__item ");
+  const menu = document.querySelector(".menu-page__list");
+  
+  menu.addEventListener("click", function(event){
+  event.preventDefault();
+  const target = event.target;
+  
+  if (target.classList.contains("menu-page__ttl")) {
+    const menuItem = target.parentNode;
+    const content = target.nextElementSibling;
+    
+  
+  for (iterator of menuItems) {
+    if (iterator !== menuItem) {
+      iterator.classList.remove("menu-page__item--active");
+      iterator.lastElementChild.style.width = 0;
+    }
+  }
+  
+  
+  if (menuItem.classList.contains("menu-page__item--active")){
+    menuItem.classList.remove("menu-page__item--active");
+    content.style.width = 0;
+  }
+  else {menuItem.classList.add("menu-page__item--active");
+  const screen = window.innerWidth;
+  console.log(screen);
+  if(screen > 768) {
+  content.style.width = 540 + "px";}
+  else {content.style.width = 340 + "px";}
+  }
+  }
+  });
+  };
+  
+  accordeonMenu();
