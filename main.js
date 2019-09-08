4901bbf7372d710fcf816c59aabff5524b65294a
yulia elkina <todg@mail.ru>
2019-09-08 18:34:08 +0300

    hw4

--- main.js
+++ main.js
@@ -29,5 +29,69 @@ close.addEventListener('click' , function(e){
 
 
 items.forEach(function(element){
-  element.addEventListener('click' , closeMenu());
+  element.addEventListener('click' , closeMenu)}
+  )
+
+
+
+function accordeonTeam() {
+const workers = document.querySelectorAll(".team__item");
+const team = document.querySelector(".team__list");
+
+team.addEventListener("click", function(event){
+event.preventDefault();
+const target = event.target;
+
+if (target.classList.contains("team__item-name")) {
+  const worker = target.parentNode;
+  const content = target.nextElementSibling;
+  const contentHeght = content.firstElementChild.clientHeight;
+
+for (iterator of workers) {
+  if (iterator !== worker) {
+    iterator.classList.remove("team__item--active");
+    iterator.firstElementChild.style.height = 0;
+  }
+}
+
+
+if (worker.classList.contains("team__item--active")){
+  worker.classList.remove("team__item--active");
+  content.style.height = 0;
+}
+else {worker.classList.add("team__item--active");
+content.style.height = contentHeight + "px";
+}
+}
+});
+}
+
+accordeonTeam();
+
+
+function popupReview() {
+  const reviewList = document.querySelector('.reviews__list');
+  reviewList.addEventListener("click" , function(event) {
+event.preventDefault();
+if (event.target.classList.contains('btn__read')) {
+  const title = event.target.parentNode.querySelector('.reviews__user-name').textContent;
+  const text = event.target.parentNode.querySelector('.reviews__text').textContent;
+
+  renderPopup(title,text);
+}
 })
+function renderPopup(title,text) {
+  const popup = document.querySelector('.popup');
+  popup.classList.add('popup--active');
+  popup.querySelector('.popup__title').textContent = title;
+  popup.querySelector('.popup__text').textContent = text;
+
+  popup.querySelector('.popup__close').addEventListener ("click", e=>{
+    e.preventDefault();
+    popup.classList.remove('popup--active');
+  })
+}
+
+}
+
+popupReview();
